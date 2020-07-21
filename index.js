8ecf19335ffd43e5c29ff155bf6efc3b33844009
Abraham Diaz <abdiaz2018@gmail.com>
2020-07-21 22:57:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
-function logWhisper(sting) {
+function logWhisper(string) {
   console.log(string.tolowercase())
 }
